7629d71c5172b8cecf648cc0e5ae32ef9c206763
ruicong chen <ruicongchen@yahoo.com>
2017-08-24 15:05:49 -0700

    hello world

--- ConsoleApplication2/ConsoleApplication2.cpp
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -1,12 +1,13 @@
 // ConsoleApplication2.cpp : Defines the entry point for the console application.
 //
 
-#include "stdafx.h"
+#include <iostream>
+using namespace std;
 
 
 int main()
 {
-	i love you
+	cout << "Hello World";
     return 0;
 }
 
